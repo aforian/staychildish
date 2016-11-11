d05ec1b819c60a6b5b27ae4bb8508e2dfa6dde8a
aforian <alexian853@gmail.com>
2016-11-11 17:05:54 +0800

    Add files via upload

--- js/staychildish.js
+++ js/staychildish.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
+    /* back top */
     $(".desktop-logo a").on('click', goto_top);
     $(".backtop").on('click', goto_top);
     function goto_top(){
         $("html,body").animate({
-                "scrollTop":$("#homepage").offset().top
+                "scrollTop":$("#homepage").offset().top-60
             },800,'easeInOutExpo');
         return false;
     }
@@ -12,34 +13,37 @@ $(document).ready(function(){
         $(".scroll-page a").eq(i).click({id:i},function(e){
             var n=e.data.id+1;
             $("html,body").stop().animate({
-                "scrollTop":$(".home-section").eq(n).offset().top},600,'easeInOutExpo');
+                "scrollTop":$(".home-section").eq(n).offset().top-60},600,'easeInOutExpo');
             return false;
         });
     }
     
+    /* right-nav */
+    for(var i=0;i<4;i++){
+        $(".right-nav ul li").eq(i).on('click',{id:i},function(e){
+            var n=e.data.id;
+            $("html,body").stop().animate({
+                "scrollTop":$(".home-section").eq(n).offset().top-60},800,'easeInOutExpo');
+        });
+    }
     $(window).scroll(function(){
         for(var i=0;i<4;i++){
             var j=i+1; 
-            if( $(window).scrollTop() >= $(".home-section").eq(i).offset().top-60 && $(window).scrollTop() < $(".home-section").eq(j).offset().top ){
+            if( $(window).scrollTop() >= $(".home-section").eq(i).offset().top-80 && $(window).scrollTop() < $(".home-section").eq(j).offset().top ){
                 $(".right-nav ul li").css({"background-color":"#454545"});
                 $(".right-nav ul li").eq(i).css({"background-color":"#676697"});
+                if(i>0){
+                    $(".backtop").addClass("active");
+                }else{
+                    $(".backtop").removeClass("active");
+                }
             }; 
         }
     });
     
     
-    for(var i=0;i<4;i++){
-        $(".right-nav ul li").eq(i).on('click',{id:i},function(e){
-            var n=e.data.id;
-            $("html,body").stop().animate({
-                "scrollTop":$(".home-section").eq(n).offset().top},800,'easeInOutExpo');
-        });
-    }
-    
-    
-    
-    
+    /* mobile-nav */
     $(".mobile-navicon").on("click",function(){
-        $(".mobile-navbar").animate({"left":"-50px"},600,'easeInOutExpo')
+        $(".mobile-navbar, .mobile-nav, .home-section, .darkcover, .right-nav").toggleClass("active");
     }); 
 })

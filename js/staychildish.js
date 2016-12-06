fcd262e1434bdabacd897f29d9f0ab2d143daa2f
aforian <alexian853@gmail.com>
2016-12-07 02:03:31 +0800

    Add files via upload

--- js/staychildish.js
+++ js/staychildish.js
@@ -1,5 +1,5 @@
   /* loading page */
-$(window).bind('load',function(){
+$(window).load(function(){
         $("#loading").fadeOut(800);
         $("#loading-icon").fadeOut(800);
     });
@@ -67,6 +67,68 @@ $(document).ready(function(){
         $("#darkcover").fadeToggle();
     }
     
+    
+    /*quiz begin & next page*/
+    var bar_width= 0, page_num=0;
+    var point, totalpoint;
+
+    $("#quiz-begin").click(function(){
+        $(".quiz-entry").fadeOut(800);
+        nextpage();
+    })
+    $("#next-question").click(function(){
+        var item = $(":radio:checked").length;
+        if(item == page_num){
+            nextpage();
+        }
+        else{
+            $(":radio").parent().css({"background-color":"#ffcbbe"});
+            $(":radio:checked").parent().css({"background-color":"#fff"});
+        }
+        
+    })
+    
+    function nextpage(){
+        $(":radio").parent().css({"background-color":"#fff"});
+        $("html,body").animate({
+            "scrollTop":$("body").offset().top
+        },400);
+        $(".quiz-question").hide();
+        for(i=page_num;i<(page_num+3);i++){
+            $(".quiz-question").eq(i).fadeIn(800);
+        }
+        page_num+=3;
+        
+        if(page_num>=$(".quiz-question").length){
+                $("#next-question").hide();
+                $("#submit").show();
+        }
+    }
+    
+    /*progress bar*/
+    for(var i=1;i<=10;i++){
+        $('input[name="q'+i+'"]:radio').change({id:i},function(e){
+            n=e.data.id;
+            progressbar();
+            $('input[name="q'+n+'"]:radio').unbind('change');
+        })
+    }
+    function progressbar(){
+        bar_width+=10;
+        $(".quiz-progress").css({"left":bar_width+"%"});
+    }
+    
+    /*count point*/
+    $("#submit").click(function(){
+        totalpoint = 0;
+        for(var i=1;i<=10;i++){
+            point = $('input[name="q'+i+'"]:checked').val();
+            totalpoint = totalpoint + Number(point);
+        }
+        alert("你的童理心指數是："+totalpoint );
+        return totalpoint;
+    });
+    
     /*owltheme*/
     $("#owl-demo").owlCarousel({
         itemsCustom : [
@@ -97,5 +159,4 @@ $(document).ready(function(){
     $(".advice-prev").click(function(){
         advice.trigger('owl.prev');
       })
-
 })
